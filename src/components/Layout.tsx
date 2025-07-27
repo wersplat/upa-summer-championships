67a96c320a6bbf2ad0a05ff538075bcdc320138e
wersplat <wersplat@gmail.com>
2025-07-27 09:42:43 -0400

    refactor: remove draft pool link from navigation menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -31,7 +31,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     { text: 'Bracket', href: '/bracket' },
     { text: 'Teams', href: '/teams' },
     { text: 'Players', href: '/players' },
-    { text: 'Draft Pool', href: '/draft-pool' },
     { text: 'Game Recaps', href: '/recap' },
     { text: 'Awards', href: '/awards' },
     { text: 'FAQ', href: '/faq' },
